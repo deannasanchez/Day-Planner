2e380455d1e5b67b1acdd238d24a2cac0c77a7d4
Dee Lynn <deannalguzman@gmail.com>
2019-10-01 20:26:24 -0700

    kind of fixed the save button

--- script.js
+++ script.js
@@ -4,33 +4,49 @@ var $plannerOutput = $(".planner-output");
 var $currentDayElem = $("#currentDay");
 
 
-var todos = [""];
+var todos = [];
 
 currentDayf();
 renderPlanner();
+init();
 
 function currentDayf () {
         var currentDay = moment();
         $currentDayElem.text(currentDay.format("MMMM Do, YYYY"));
 };
 
-
-
 function renderPlanner () {
-    $plannerOutput.html(" ");
+    $plannerOutput.append(" ");
 
     for (var i = 0; i < todos.length; i++) {
         var todo = todos[i];
-        var td = $("<td>", "td")
-        td.text(todo);
-        $plannerOutput.append(td);
+        //var $targetID = $("#"+stringify)
+        // var td = $("<td>")
+        $plannerOutput.append(todo);
+        // $plannerOutput.attr(td);
+        //var $targetid = $("#"+stringify(todo.time))
       }
 };
 
-saveBtn.on("click", function(event) {
+  function init() {
+      var plansStr = localStorage.getItem("todos");
+    //   if (!(plansStr === null || plansStr === "")){
+    //       todos = JSON.stringify(plansStr);
+    //   }
+      storePlans();
+      renderPlanner();
+  }
+
+  function storePlans() {
+      localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  saveBtn.on("click", function(event) {
     event.preventDefault();
-    var plan = plannerText.val();
-    plan = plan.trim();
+    localStorage.clear()
+    var plan = plannerText.val().trim();
+    console.log(plan)
     todos.push(plan);
     renderPlanner();
+    storePlans();
   });
